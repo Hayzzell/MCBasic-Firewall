40033c7491595d4accd882a454704ad2d80f8e8b
Hayzzell <darkline222244@gmail.com>
2024-06-22 20:12:31 +0400

    Updated

--- mitigation.sh
+++ mitigation.sh
@@ -4,7 +4,7 @@
 # https://Pixiemines.com/
 
 
-function RootChekcer() {
+function Rootchekcer() {
 	if [ "${EUID}" -ne 0 ]; then
 		echo "You have to run the script as root"
 		exit 1
@@ -49,7 +49,7 @@ function OSVersionChecker() {
 }
 
 
-function checkcer() {
+function checkcer(){
 Rootchekcer
 OSVersionChecker
 }
@@ -78,6 +78,7 @@ done
 
 
  if [[ "$UDP_STATE" == "OFF" ]]; then
+    # Command to turn off UDP
     echo "Turning off UDP..."
     #  Here it executes the turn off comamand, if you wish to keep DNS on or anyother UDP port please add it in here(make sure its above the DROP). 
     iptables -A INPUT -p udp -j DROP
@@ -89,3 +90,160 @@ else
 
 }
 
+function Applying(){
+
+# Anti-DDoS Kernel Settings
+sysctl_setting="
+
+kernel.printk = 4 4 1 7
+kernel.panic = 10
+kernel.sysrq = 0
+kernel.shmmax = 4294967296
+kernel.shmall = 4194304
+kernel.core_uses_pid = 1
+kernel.msgmnb = 65536
+kernel.msgmax = 65536
+vm.swappiness = 20
+vm.dirty_ratio = 80
+vm.dirty_background_ratio = 5
+fs.file-max = 2097152
+net.core.netdev_max_backlog = 262144
+net.core.rmem_default = 31457280
+net.core.rmem_max = 67108864
+net.core.wmem_default = 31457280
+net.core.wmem_max = 67108864
+net.core.somaxconn = 4096
+net.core.optmem_max = 25165824
+net.ipv4.neigh.default.gc_thresh1 = 4096
+net.ipv4.neigh.default.gc_thresh2 = 8192
+net.ipv4.neigh.default.gc_thresh3 = 16384
+net.ipv4.neigh.default.gc_interval = 5
+net.ipv4.neigh.default.gc_stale_time = 120
+net.netfilter.nf_conntrack_max = 10000000
+net.netfilter.nf_conntrack_buckets = 65536
+net.netfilter.nf_conntrack_tcp_loose = 0
+net.netfilter.nf_conntrack_tcp_timeout_established = 1800
+net.netfilter.nf_conntrack_tcp_timeout_close = 10
+net.netfilter.nf_conntrack_tcp_timeout_close_wait = 10
+net.netfilter.nf_conntrack_tcp_timeout_fin_wait = 20
+net.netfilter.nf_conntrack_tcp_timeout_last_ack = 20
+net.netfilter.nf_conntrack_tcp_timeout_syn_recv = 20
+net.netfilter.nf_conntrack_tcp_timeout_syn_sent = 20
+net.netfilter.nf_conntrack_tcp_timeout_time_wait = 10
+net.ipv4.tcp_slow_start_after_idle = 0
+net.ipv4.ip_local_port_range = 1024 65000
+net.ipv4.ip_no_pmtu_disc = 1
+net.ipv4.route.flush = 1
+net.ipv4.route.max_size = 8048576
+net.ipv4.icmp_echo_ignore_broadcasts = 1
+net.ipv4.icmp_ignore_bogus_error_responses = 1
+net.ipv4.tcp_congestion_control = htcp
+net.ipv4.tcp_mem = 65536 131072 262144
+net.ipv4.udp_mem = 65536 131072 262144
+net.ipv4.tcp_rmem = 4096 87380 33554432
+net.ipv4.udp_rmem_min = 16384
+net.ipv4.tcp_wmem = 4096 87380 33554432
+net.ipv4.udp_wmem_min = 16384
+net.ipv4.tcp_max_tw_buckets = 1440000
+net.ipv4.tcp_tw_recycle = 0
+net.ipv4.tcp_tw_reuse = 1
+net.ipv4.tcp_max_orphans = 400000
+net.ipv4.tcp_window_scaling = 1
+net.ipv4.tcp_rfc1337 = 1
+net.ipv4.tcp_syncookies = 1
+net.ipv4.tcp_synack_retries = 1
+net.ipv4.tcp_syn_retries = 2
+net.ipv4.tcp_max_syn_backlog = 32768
+net.ipv4.tcp_timestamps = 1
+net.ipv4.tcp_sack = 1
+net.ipv4.tcp_fack = 1
+net.ipv4.tcp_ecn = 2
+net.ipv4.tcp_fin_timeout = 10
+net.ipv4.tcp_keepalive_time = 600
+net.ipv4.tcp_keepalive_intvl = 60
+net.ipv4.tcp_keepalive_probes = 10
+net.ipv4.tcp_no_metrics_save = 1
+net.ipv4.ip_forward = 1
+net.ipv4.conf.all.accept_redirects = 0
+net.ipv4.conf.all.send_redirects = 0
+net.ipv4.conf.all.accept_source_route = 0
+net.ipv4.conf.all.rp_filter = 1
+net.ipv4.conf.all.secure_redirects = 0
+net.ipv4.conf.default.secure_redirects = 0
+net.ipv4.conf.all.log_martians = 1
+net.ipv4.conf.default.log_martians = 1
+net.ipv4.ipfrag_low_thresh = 196608
+net.ipv4.ipfrag_high_thresh = 262144
+net.ipv4.tcp_synproxy_enabled = 1
+net.ipv4.icmp_ratelimit = 1000
+
+"
+
+# Check if the setting already exists in the file
+if grep -q "^${sysctl_setting%%=*}" /etc/sysctl.conf; then
+    # If it exists, replace the line
+    sudo sed -i "s/^${sysctl_setting%%=*}.*/$sysctl_setting/" /etc/sysctl.conf
+else
+    echo "$sysctl_setting" | sudo tee -a /etc/sysctl.conf > /dev/null
+fi
+
+sudo sysctl -p
+
+
+
+# extras
+
+### 1: Drop invalid packets ### 
+/sbin/iptables -t mangle -A PREROUTING -i eth0 -m conntrack --ctstate INVALID -j DROP  
+
+
+### 2: Drop TCP packets that are new and are not SYN ### 
+/sbin/iptables -t mangle -A PREROUTING -i eth0 -p tcp ! --syn -m conntrack --ctstate NEW -j DROP 
+
+ 
+### 3: Drop SYN packets with suspicious MSS value ### 
+/sbin/iptables -t mangle -A PREROUTING -i eth0 -p tcp -m conntrack --ctstate NEW -m tcpmss ! --mss 536:65535 -j DROP  
+
+
+### 4: Block packets with bogus TCP flags ### 
+/sbin/iptables -t mangle -A PREROUTING -i eth0 -p tcp --tcp-flags FIN,SYN FIN,SYN -j DROP
+/sbin/iptables -t mangle -A PREROUTING -i eth0 -p tcp --tcp-flags SYN,RST SYN,RST -j DROP
+/sbin/iptables -t mangle -A PREROUTING -i eth0 -p tcp --tcp-flags FIN,RST FIN,RST -j DROP
+/sbin/iptables -t mangle -A PREROUTING -i eth0 -p tcp --tcp-flags FIN,ACK FIN -j DROP
+/sbin/iptables -t mangle -A PREROUTING -i eth0 -p tcp --tcp-flags ACK,URG URG -j DROP
+/sbin/iptables -t mangle -A PREROUTING -i eth0 -p tcp --tcp-flags ACK,PSH PSH -j DROP
+/sbin/iptables -t mangle -A PREROUTING -i eth0 -p tcp --tcp-flags ALL NONE -j DROP
+
+
+### 6: Drop ICMP (you usually don't need this protocol) ### 
+/sbin/iptables -t mangle -A PREROUTING -i eth0 -p icmp -j DROP  
+
+
+### 7: Drop fragments in all chains ### 
+/sbin/iptables -t mangle -A PREROUTING -i eth0 -f -j DROP  
+
+
+### 8: Limit connections per source IP ### 
+/sbin/iptables -A INPUT -p tcp -m connlimit --connlimit-above 111 -j REJECT --reject-with tcp-reset  
+
+
+### 9: Limit RST packets ### 
+/sbin/iptables -A INPUT -p tcp --tcp-flags RST RST -m limit --limit 2/s --limit-burst 2 -j ACCEPT 
+/sbin/iptables -A INPUT -p tcp --tcp-flags RST RST -j DROP  
+
+
+### 10: Limit new TCP connections per second per source IP ### 
+/sbin/iptables -A INPUT -p tcp -m conntrack --ctstate NEW -m limit --limit 60/s --limit-burst 20 -j ACCEPT 
+/sbin/iptables -A INPUT -p tcp -m conntrack --ctstate NEW -j DROP  
+ 
+### SSH brute-force protection ### 
+/sbin/iptables -A INPUT -p tcp --dport ssh -m conntrack --ctstate NEW -m recent --set 
+/sbin/iptables -A INPUT -p tcp --dport ssh -m conntrack --ctstate NEW -m recent --update --seconds 60 --hitcount 10 -j DROP  
+
+
+### Protection against port scanning ### 
+/sbin/iptables -N port-scanning 
+/sbin/iptables -A port-scanning -p tcp --tcp-flags SYN,ACK,FIN,RST RST -m limit --limit 1/s --limit-burst 2 -j RETURN 
+/sbin/iptables -A port-scanning -j DROP
+
+}
